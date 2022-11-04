998deae89360f9f6cf778c9396f71164912ac913
Verkin78 <otrajenie.jc@gmail.com>
2022-11-04 12:27:06 +0300

    hw4.3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //task4.1//
-        int age = 18;
+        int age = 1;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
         }
-
         if (age < 18) {
             System.out.println("Возраст совершеннолетия ещё не наступил, вам нужно подождать");
         }
@@ -17,6 +16,13 @@ public class Main {
         if (ageOfKid >= 7) {System.out.println("ребенок ходит в школу");}
         if (ageOfStudent >= 18) {System.out.println("уже закончил школу и может отправляться в университет");}
         if (ageOfYouth >= 24) {System.out.println("окончил университет и пора искать первую работу");}
+
+        //task4.3//
+        int place = 88;
+        if (place <= 59) {System.out.println("В вагоне есть сидячие и стоячие места");}
+        if (place >= 60 && place <= 102) {System.out.println("В вагоне есть только стоячие места");}
+        if (place >= 102) {System.out.println("В вагоне нет свободных мест");}
+
 
     }
 }
